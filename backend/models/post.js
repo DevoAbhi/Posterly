efcfdb6ab37de965daa49747ddea624749bd7022
AbhiDroid <abhinabroy2001@gmail.com>
2021-01-01 17:38:27 +0530

    Create post instance using moongoose model

--- backend/models/post.js
+++ backend/models/post.js
@@ -1,8 +1,8 @@
 const mongoose = require('mongoose');
 
 const postSchema = mongoose.Schema({
-  title: { title: String, required: true},
-  content: { title: String, required: true}
+  title: { type: String, required: true},
+  content: { type: String, required: true}
 });
 
 module.exports = mongoose.model('Post', postSchema);
